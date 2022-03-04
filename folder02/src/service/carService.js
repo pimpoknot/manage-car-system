06ca4ad9db55aefb4c8f3a2c678ad23912148795
Marcos Maverick <marquinhopimpo@gmail.com>
2022-03-03 22:31:05 -0300

    finishing test with carRepository

--- folder02/src/service/carService.js
+++ folder02/src/service/carService.js
@@ -1,12 +1,31 @@
 const BaseRepository = require('./../repository/base/baseRepository')
 
 class CarService {
-    constructor({car}) {
-        this.carRepository = new CarRepository({ file: car })
+    constructor({cars}) {
+        this.carRepository = new BaseRepository({ file: cars })
     }
 
-    test() {
-        return this.carRepository.find()
+    getRandomPositionFromArray(list) {
+        const listLength = list.length
+        return Math.floor(
+            Math.random() * (listLength)
+        )
+    }
+    chooseRandomCar(carCategory) {
+        const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
+        const carId = carCategory.carIds[randomCarIndex]
+
+        return carId
+    }
+
+    async getAvaibleCar(carCategory) {
+        const carId = this.chooseRandomCar(carCategory)
+        const car = await this.carRepository.find(carId)
+        return car 
+    }
+    
+    test(id) {
+        return this.carRepository.find(id)
     }
 }
 
